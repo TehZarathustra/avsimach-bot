fc12163946fd96cefcbc90bdfdd281f9e29765d8
v-prusakov <v-prusakov@yandex-team.ru>
2018-05-03 17:55:16 +0300

    allow cors

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
-app.get('/', function (request, response) {
+app.get('/', function (req, res) {
 	response.send('avsimach telegraf bot');
 });
 
-app.get('/testkek', function (request, response) {
+app.get('/testkek', function (req, res) {
 	console.log('in keks >>>');
 	response.send('keks is working!');
 });
